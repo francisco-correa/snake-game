4094b1a78c4c685a117f9ec7209aabdc542e414c
Francisco Correa <fcorrea.fernandez@gmail.com>
2021-06-19 23:32:33 -0400

    adding a game lose condition

--- snake.py
+++ snake.py
@@ -1,6 +1,6 @@
 import turtle
-import random
 import time
+import random
 
 delay = 0.1
 score = 0
@@ -25,7 +25,6 @@ food.shape("circle")
 food.penup()
 food.goto(50,50)
 
-
 body = []
 
 def go_up():
@@ -43,16 +42,16 @@ def go_left():
 def move():
     if snake.direction == "up":
         y = snake.ycor()
-        snake.sety(y + 10)
+        snake.sety(y + 15)
     if snake.direction == "down":
         y = snake.ycor()
-        snake.sety(y - 10)
+        snake.sety(y - 15)
     if snake.direction == "right":
         x = snake.xcor()
-        snake.setx(x + 10)
+        snake.setx(x + 15)
     if snake.direction == "left":
         x = snake.xcor()
-        snake.setx(x - 10)
+        snake.setx(x - 15)
 
 screen.listen()
 screen.onkeypress(go_up, "Up")
@@ -62,12 +61,48 @@ screen.onkeypress(go_left, "Left")
 
 while True:
     screen.update()
-    if snake.distance(food) < 10:
-        x = random.randint(-200, 200)
-        y = random.randint(-200, 200)
-        food.goto(x, y)
+    if snake.xcor() > 250 or snake.xcor() < -250 or snake.ycor() > 250 or snake.ycor() < -250:
+        time.sleep(1)
+        snake.goto(0,0)
+        snake.direction = "stop"
 
+        for x in body:
+            x.goto(1000, 1000)
+        body.clear()
+
+    if snake.distance(food) < 20:
+        x = random.randint(-250, 250)
+        y = random.randint(-250, 250)
+        food.goto(x, y)
+    
+        new_body = turtle.Turtle()
+        new_body.shape("square")
+        new_body.color("grey")
+        new_body.speed(0)
+        new_body.penup()
+        body.append(new_body)
+    
+    for i in range(len(body)-1, 0, -1):
+        x = body[i - 1].xcor()
+        y = body[i - 1].ycor()
+        body[i].goto(x, y)
+    
+    if len(body) > 0:
+        x = snake.xcor()
+        y = snake.ycor()
+        body[0].goto(x, y)
     move()
+
+    # for x in body:
+    #     if x.distance(body) < 20:
+    #         time.sleep(1)
+    #         snake.goto(0,0)
+    #         snake.direction = "stop"
+
+    #         for x in body:
+    #             x.goto(1000, 1000)
+    #         body.clear()
+
     time.sleep(delay)
 
 # turtle.done()
